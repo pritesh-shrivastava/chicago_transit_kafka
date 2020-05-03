4cd6602633564ffad4bfdfaa89b7db2701607a26
pritesh-shrivastava <pritesh2312@gmail.com>
2020-05-03 13:31:45 +0530

    [PS] Fix typos

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -30,9 +30,7 @@ class Turnstile(Producer):
             .replace("-", "_")
             .replace("'", "")
         )
-
-        #
-        #
+        
         # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
         # replicas
         #
@@ -40,9 +38,9 @@ class Turnstile(Producer):
         super().__init__(
             topic_name, # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
-            value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
+            value_schema=Turnstile.value_schema, 
             num_partitions=3,
-            num_replicas=2,
+            num_replicas=2
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
